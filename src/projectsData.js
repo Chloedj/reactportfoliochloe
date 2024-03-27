67559239a625f1ebc6a335478cc57f7399443e81
Chloedj <155569174+Chloedj@users.noreply.github.com>
2024-03-27 18:54:42 +0000

    Added project data

---
+++ src/projectsData.js
@@ -0,0 +1,53 @@
+const projectsData = [
+  {
+    id: 1,
+    title: "Modern UI/UX Responsive Site",
+    description: "a Fully Responsive Website with Modern UI/UX in React JS with Tailwind",
+    imageUrl: "src/images/Brainwave.png",
+    link: "https://clinquant-paletas-4cd98f.netlify.app/",
+    technologies: ["fa-html5", "fa-css3-alt", "fa-react"],
+  },
+  {
+    id: 2,
+    title: "React Recipe App",
+    description: "This project was built using React, and fetches a wide range of recipes for different dishes. All the logic of recipe research, getting the results, and displaying them in the card format is implemented using JSX.",
+    imageUrl: "src/images/recipeapp.png",
+    link: "https://beamish-beijinho-c87228.netlify.app/",
+    technologies: ["fa-html5", "fa-css3-alt", "fa-react"],
+  },
+  {
+    id: 3,
+    title: "React Weather App",
+    description: "This project was built using React, and fetches real-time weather data. It's an example of integrating live APIs and managing code with Git and GitHub.",
+    imageUrl: "src/images/reactweatherapp.png",
+    link: "https://splendorous-kulfi-ea79b5.netlify.app/",
+    technologies: ["fa-html5", "fa-css3-alt", "fa-react"],
+  },
+  {
+    id: 4,
+    title: "React Dictionary",
+    description: "Another React project, demonstrating the use of multiple API's.",
+    imageUrl: "src/images/reactdictionary.png",
+    link: "https://glittering-kleicha-f8d802.netlify.app/",
+    technologies: ["fa-html5", "fa-css3-alt", "fa-react"],
+  },
+  {
+    id: 5,
+    title: "Responsive Travel Site",
+    description: "A responsive project with more sophisticated design and advanced CSS techniques.",
+    imageUrl: "src/images/responsivetravelsite.png",
+    link: "https://roaring-dango-ef5723.netlify.app/",
+    technologies: ["fa-html5", "fa-css3-alt", "fa-mobile-screen"],
+  },
+  {
+    id: 6,
+    title: "World Clock",
+    description: "Showcasing my skills in JavaScript and API handling, this app features a global clock functionality with sophisticated interactions.",
+    imageUrl: "src/images/worldclock.png",
+    link: "https://dashing-fairy-4c0a4d.netlify.app/",
+    technologies: ["fa-html5", "fa-css3-alt", "fa-js"],
+  },
+  
+];
+
+export default projectsData;
